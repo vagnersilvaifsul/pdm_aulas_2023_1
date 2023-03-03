f82f520c5fab6d88354ee639a6c3fa979b7d2e1e
Vagner Pinto <vagnersilva@ifsul.edu.br>
2023-03-03 10:43:27 -0300

    feat: navigation

---
+++ src/screens/Preload/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const Preload = () => {
+  return (
+    <View>
+      <Text>Preload</Text>
+    </View>
+  );
+};
+
+export default Preload;
---
+++ src/screens/SignUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>Preload</Text>
+    </View>
+  );
+};
+
+export default SignUp;
