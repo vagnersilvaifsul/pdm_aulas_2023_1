cbb2d0a34e4b6df13e1479a1e0a8b86547fbd112
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2023-05-27 15:48:51 -0300

    feat: notifications listeners run

--- src/context/Notifications.js
+++ src/context/Notifications.js
@@ -5,11 +5,11 @@ import messaging from '@react-native-firebase/messaging';
 export const NotificationsContext = createContext({});
 
 export const NotificationsProvider = ({children}) => {
-  const [notificationContent, setNotificationContent] = useState(null);
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     /*
-      Inscreve os listeners dos 3 tipos de processamento de notifications.
+      Inscreve os listeners para os 3 tipos de processamento de notifications.
       Obs.: Para que estes listeners funcionem, no arquivo index.js (na raiz do app) você
       deve registrar o messaging().setBackgroundMessageHandler(async remoteMessage => {}).
     */
@@ -18,53 +18,50 @@ export const NotificationsProvider = ({children}) => {
     messaging()
       .getInitialNotification()
       .then(remoteMessage => {
+        console.log(
+          'Notification recebida com o app fechado (activity onDestroy): ',
+          remoteMessage,
+        );
         if (remoteMessage) {
-          console.log(
-            'Notification recebida com o app fechado (objeto destruído): ',
-            remoteMessage,
-          );
+          setNotification(remoteMessage);
+          //TODO: abrir a notification na página correta
         }
       });
 
     //quando o app está parado (testado, ok)
     messaging().onNotificationOpenedApp(remoteMessage => {
-      if (remoteMessage.notification.title && remoteMessage.notification.body) {
-        let content = {
-          title: remoteMessage.notification.title,
-          body: remoteMessage.notification.body,
-        };
-        setNotificationContent(content);
+      console.log(
+        'Notification recebida com o app parado (activity onStop): ',
+        remoteMessage,
+      );
+      if (remoteMessage) {
+        setNotification(remoteMessage);
+        //TODO: abrir a notification na página correta
       }
-      //navigate(remoteMessage.data.route);
     });
 
     //quando o app está aberto (testado, ok)
     messaging().onMessage(async remoteMessage => {
-      console.log('Notification recebida com o app aberto: ', remoteMessage);
-      if (remoteMessage.notification.title && remoteMessage.notification.body) {
-        let content = {
-          title: remoteMessage.notification.title,
-          body: remoteMessage.notification.body,
-        };
-        setNotificationContent(content);
-      }
-      switch (remoteMessage.data.route) {
-        case 'admin':
-          Alert.alert(
-            'Notificação',
-            'Notícia: ' + remoteMessage.notification.body,
-            [
-              {text: 'ir', onPress: () => {}},
+      console.log(
+        'Notification recebida com o app aberto (activity na tela): ',
+        remoteMessage,
+      );
+      if (remoteMessage) {
+        setNotification(remoteMessage);
+        switch (remoteMessage.data.route) {
+          case 'admin':
+            Alert.alert('admin', 'Tópico: ' + remoteMessage.data.route, [
+              {text: 'ir', onPress: () => {}}, //TODO: abrir a notification na página correta
               {text: 'não', onPress: () => {}},
-            ],
-          );
-          break;
-        case 'user':
-          Alert.alert('user', 'Campanha: ' + remoteMessage.notification.body, [
-            {text: 'ir', onPress: () => {}},
-            {text: 'não', onPress: () => {}},
-          ]);
-          break;
+            ]);
+            break;
+          case 'user':
+            Alert.alert('user', 'Tópico: ' + remoteMessage.data.route, [
+              {text: 'ir', onPress: () => {}}, //TODO: abrir a notification na página correta
+              {text: 'não', onPress: () => {}},
+            ]);
+            break;
+        }
       }
     });
   }, []);
